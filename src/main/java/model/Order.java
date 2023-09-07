18a62e3159a7a931946039e6847165dc82885f38
LAB_PC <smadankar2004@gmail.com>
2023-09-07 16:21:48 +0530

    model classes created

---
+++ src/main/java/model/Order.java
@@ -0,0 +1,51 @@
+package model;
+
+import java.util.Date;
+
+public class Order {
+    int id;
+    String deliveryAddress;
+    Date orderDate;
+    double amount;
+    String restaurantName;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getDeliveryAddress() {
+        return deliveryAddress;
+    }
+
+    public void setDeliveryAddress(String deliveryAddress) {
+        this.deliveryAddress = deliveryAddress;
+    }
+
+    public Date getOrderDate() {
+        return orderDate;
+    }
+
+    public void setOrderDate(Date orderDate) {
+        this.orderDate = orderDate;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+
+    public String getRestaurantName() {
+        return restaurantName;
+    }
+
+    public void setRestaurantName(String restaurantName) {
+        this.restaurantName = restaurantName;
+    }
+}
